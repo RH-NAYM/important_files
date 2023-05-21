e964315e39cdb2ed57fa22949545cd2e9bd1c284
RH-NAYM <73577646+RH-NAYM@users.noreply.github.com>
2023-05-21 18:22:37 +0600

    Updated run_api.py from BEAST
    Updated the code to take url and sequence of images and run the detection and print result

--- run_api.py
+++ run_api.py
@@ -1,22 +1,21 @@
 from fastapi import FastAPI
-import asyncio
-from colorama import init
-from colorama import Fore, Back, Style
-from fastapi import FastAPI
 from pydantic import BaseModel
 import asyncio
-from run_detection_function import det
+from function import det
+import json
 
 app = FastAPI()
 
 class Item(BaseModel):
     url: str
+    sequence: list
+
 @app.post("/items/")
 async def create_item(item: Item):
     url = item.url
-    a = await asyncio.create_task(det(url))
-    return a
+    sequence = item.sequence
+    # sequence = json.dumps(sequence)
+    result = await asyncio.create_task(det(url, sequence))
+    final_result_json = json.loads(result)
+    return final_result_json
 
-
-
-# print(result)
