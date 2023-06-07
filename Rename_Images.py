8ef1a8a7b3653d81f0893d107fbd2131095edbb1
RH-NAYM <73577646+RH-NAYM@users.noreply.github.com>
2023-06-07 11:09:55 +0600

    Create Rename_Images.py Created from BEAST
    A file that will rename all the files to my desired one and add increment counting

---
+++ Rename_Images.py
@@ -0,0 +1,13 @@
+# Rename All Images
+import os
+def main():
+	i = 0
+	path="Old/"
+	for filename in os.listdir(path):
+		my_dest ="HairCare_New_Images" + str(i) + ".jpg"
+		my_source =path + filename
+		my_dest =path + my_dest
+		os.rename(my_source, my_dest)
+		i += 1
+if __name__ == '__main__':
+	main()
