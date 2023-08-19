4fd2b7a98718f305e3f62fcb80423cd26e9bd26d
MD Rakibul Hasan Naym <73577646+RH-NAYM@users.noreply.github.com>
2023-08-19 15:36:30 +0600

    Add files via upload

---
+++ label_studio_data_Split.py
@@ -0,0 +1,456 @@
+data = [
+    {
+      "id": 0,
+      "name": "bkash_board_back_lit_1_with_image"
+    },
+    {
+      "id": 1,
+      "name": "bkash_festoon_bill_pay"
+    },
+    {
+      "id": 2,
+      "name": "bkash_festoon_digital"
+    },
+    {
+      "id": 3,
+      "name": "bkash_festoon_price"
+    },
+    {
+      "id": 4,
+      "name": "bkash_festoon_qr"
+    },
+    {
+      "id": 5,
+      "name": "bkash_festoon_remittance"
+    },
+    {
+      "id": 6,
+      "name": "bkash_festoon_savings"
+    },
+    {
+      "id": 7,
+      "name": "bkash_hanger_push-pull_door"
+    },
+    {
+      "id": 8,
+      "name": "bkash_hanging_board_open-close"
+    },
+    {
+      "id": 9,
+      "name": "bkash_pos_pointer_back_led"
+    },
+    {
+      "id": 10,
+      "name": "bkash_poster_islamic"
+    },
+    {
+      "id": 11,
+      "name": "bkash_poster_mobile_recharge"
+    },
+    {
+      "id": 12,
+      "name": "bkash_poster_remittance"
+    },
+    {
+      "id": 13,
+      "name": "bkash_poster_savings"
+    },
+    {
+      "id": 14,
+      "name": "bkash_poster_send_money"
+    },
+    {
+      "id": 15,
+      "name": "bkash_poster_table_messi"
+    },
+    {
+      "id": 16,
+      "name": "bkash_poster_western_union"
+    },
+    {
+      "id": 17,
+      "name": "bkash_shop_banner_price"
+    },
+    {
+      "id": 18,
+      "name": "bkash_sticker_bill_pay_table"
+    },
+    {
+      "id": 19,
+      "name": "bkash_sticker_fraud_awareness"
+    },
+    {
+      "id": 20,
+      "name": "bkash_sticker_mask_bangla"
+    },
+    {
+      "id": 21,
+      "name": "bkash_sticker_mask_english"
+    },
+    {
+      "id": 22,
+      "name": "bkash_sticker_payment_dai_cut"
+    },
+    {
+      "id": 23,
+      "name": "bkash_sticker_payment_large"
+    },
+    {
+      "id": 24,
+      "name": "bkash_sticker_payment_running"
+    },
+    {
+      "id": 25,
+      "name": "bkash_sticker_price"
+    },
+    {
+      "id": 26,
+      "name": "bkash_sticker_push-pull_door"
+    },
+    {
+      "id": 27,
+      "name": "bkash_sticker_qr_code"
+    },
+    {
+      "id": 28,
+      "name": "bkash_sticker_qr_vertical_1"
+    },
+    {
+      "id": 29,
+      "name": "bkash_sticker_running"
+    },
+    {
+      "id": 30,
+      "name": "bkash_sticker_shutter"
+    },
+    {
+      "id": 31,
+      "name": "bkash_sticker_table"
+    },
+    {
+      "id": 32,
+      "name": "bkash_sticker_transaction"
+    },
+    {
+      "id": 33,
+      "name": "bkash_table_top_qr_pvc_dai_cut"
+    },
+    {
+      "id": 34,
+      "name": "bkash_table_top_qr_pvc_running"
+    },
+    {
+      "id": 35,
+      "name": "bkash_table_top_qr_smart"
+    },
+    {
+      "id": 36,
+      "name": "nagad_back_lit_2"
+    },
+    {
+      "id": 37,
+      "name": "nagad_board_back_lit_1_with_picture"
+    },
+    {
+      "id": 38,
+      "name": "nagad_board_open_close_pvc"
+    },
+    {
+      "id": 39,
+      "name": "nagad_festoon_bmet"
+    },
+    {
+      "id": 40,
+      "name": "nagad_festoon_indian_visa"
+    },
+    {
+      "id": 41,
+      "name": "nagad_festoon_islamic"
+    },
+    {
+      "id": 42,
+      "name": "nagad_festoon_lenden_1"
+    },
+    {
+      "id": 43,
+      "name": "nagad_festoon_lenden_2"
+    },
+    {
+      "id": 44,
+      "name": "nagad_festoon_nagad_sheba"
+    },
+    {
+      "id": 45,
+      "name": "nagad_festoon_price_1"
+    },
+    {
+      "id": 46,
+      "name": "nagad_festoon_price_2"
+    },
+    {
+      "id": 47,
+      "name": "nagad_festoon_remittance"
+    },
+    {
+      "id": 48,
+      "name": "nagad_festoon_send_money"
+    },
+    {
+      "id": 49,
+      "name": "nagad_hanging_display"
+    },
+    {
+      "id": 50,
+      "name": "nagad_identifier_nagad"
+    },
+    {
+      "id": 51,
+      "name": "nagad_ms_standee"
+    },
+    {
+      "id": 52,
+      "name": "nagad_poster_fraud_awareness_1"
+    },
+    {
+      "id": 53,
+      "name": "nagad_poster_fraud_awareness_2"
+    },
+    {
+      "id": 54,
+      "name": "nagad_poster_islamic"
+    },
+    {
+      "id": 55,
+      "name": "nagad_poster_mobile_recharge"
+    },
+    {
+      "id": 56,
+      "name": "nagad_poster_remittance_2"
+    },
+    {
+      "id": 57,
+      "name": "nagad_shop_banner_customized"
+    },
+    {
+      "id": 58,
+      "name": "nagad_shop_banner_lenden"
+    },
+    {
+      "id": 59,
+      "name": "nagad_shop_banner_nagad_sheba"
+    },
+    {
+      "id": 60,
+      "name": "nagad_shop_banner_price_1"
+    },
+    {
+      "id": 61,
+      "name": "nagad_shop_banner_price_2"
+    },
+    {
+      "id": 62,
+      "name": "nagad_sticker_customized_table"
+    },
+    {
+      "id": 63,
+      "name": "nagad_sticker_fraud_awareness"
+    },
+    {
+      "id": 64,
+      "name": "nagad_sticker_hotline"
+    },
+    {
+      "id": 65,
+      "name": "nagad_sticker_hotline_file_folder"
+    },
+    {
+      "id": 66,
+      "name": "nagad_sticker_payment"
+    },
+    {
+      "id": 67,
+      "name": "nagad_sticker_payment_accepted_here"
+    },
+    {
+      "id": 68,
+      "name": "nagad_sticker_push_pull_door"
+    },
+    {
+      "id": 69,
+      "name": "nagad_sticker_qr_code"
+    },
+    {
+      "id": 70,
+      "name": "nagad_sticker_qr_horizontal_new"
+    },
+    {
+      "id": 71,
+      "name": "nagad_sticker_qr_horizontal_old"
+    },
+    {
+      "id": 72,
+      "name": "nagad_sticker_qr_vertical"
+    },
+    {
+      "id": 73,
+      "name": "nagad_sticker_remittance_1_large"
+    },
+    {
+      "id": 74,
+      "name": "nagad_sticker_remittance_2"
+    },
+    {
+      "id": 75,
+      "name": "nagad_sticker_running"
+    },
+    {
+      "id": 76,
+      "name": "nagad_sticker_shutter"
+    },
+    {
+      "id": 77,
+      "name": "nagad_sticker_table_1"
+    },
+    {
+      "id": 78,
+      "name": "nagad_sticker_table_2"
+    },
+    {
+      "id": 79,
+      "name": "nagad_sticker_table_3"
+    },
+    {
+      "id": 80,
+      "name": "nagad_table_talker"
+    },
+    {
+      "id": 81,
+      "name": "nagad_table_top_qr_pvc"
+    },
+    {
+      "id": 82,
+      "name": "nagad_table_top_qr_wooden_base_new"
+    },
+    {
+      "id": 83,
+      "name": "nagad_table_top_qr_wooden_base_old"
+    },
+    {
+      "id": 84,
+      "name": "nagad_x_banner"
+    },
+    {
+      "id": 85,
+      "name": "rocket_festoon_bill_pay"
+    },
+    {
+      "id": 86,
+      "name": "rocket_festoon_lenden"
+    },
+    {
+      "id": 87,
+      "name": "rocket_festoon_price"
+    },
+    {
+      "id": 88,
+      "name": "rocket_festoon_send_money"
+    },
+    {
+      "id": 89,
+      "name": "rocket_poster_add_money"
+    },
+    {
+      "id": 90,
+      "name": "rocket_poster_isnsurance"
+    },
+    {
+      "id": 91,
+      "name": "rocket_shop_banner_bill_pay"
+    },
+    {
+      "id": 92,
+      "name": "rocket_shop_banner_lenden"
+    },
+    {
+      "id": 93,
+      "name": "rocket_shop_banner_price"
+    },
+    {
+      "id": 94,
+      "name": "rocket_shop_banner_send_money"
+    },
+    {
+      "id": 95,
+      "name": "rocket_sticker_qr_code"
+    },
+    {
+      "id": 96,
+      "name": "rocket_sticker_running"
+    },
+    {
+      "id": 97,
+      "name": "rocket_sticker_shutter"
+    },
+    {
+      "id": 98,
+      "name": "rocket_sticker_table"
+    },
+    {
+      "id": 99,
+      "name": "rocket_sticker_table_price"
+    },
+    {
+      "id": 100,
+      "name": "rocket_table_top_qr_1_pvc"
+    },
+    {
+      "id": 101,
+      "name": "tap_table_top_qr"
+    },
+    {
+      "id": 102,
+      "name": "upay_festoon_price"
+    },
+    {
+      "id": 103,
+      "name": "upay_festoon_traffic_1"
+    },
+    {
+      "id": 104,
+      "name": "upay_festoon_traffic_2"
+    },
+    {
+      "id": 105,
+      "name": "upay_poster_lenden"
+    },
+    {
+      "id": 106,
+      "name": "upay_shop_banner"
+    },
+    {
+      "id": 107,
+      "name": "upay_sticker_qr_code_1"
+    },
+    {
+      "id": 108,
+      "name": "upay_sticker_qr_code_2"
+    },
+    {
+      "id": 109,
+      "name": "upay_sticker_shutter"
+    },
+    {
+      "id": 110,
+      "name": "upay_table_top_qr_pvc"
+    }
+  ]
+
+
+
+for item in data:
+    for i,j in item.items():
+        if i=="id":
+            id = j
+        elif i=="name":
+            name = j
+    print(id,":",name)
