60d056af0cee637d9d1c2e08bddc58cde02956a0
MD Rakibul Hasan Naym <73577646+RH-NAYM@users.noreply.github.com>
2024-02-09 21:50:23 +0600

    Update windows_core_counter.py

--- windows_core_counter.py
+++ windows_core_counter.py
@@ -8,3 +8,21 @@ def get_number_of_workers():
 if __name__ == "__main__":
     num_workers = get_number_of_workers()
     print("Number of workers : ",num_workers)
+
+
+"""
+import multiprocessing
+
+def get_number_of_workers():
+    # Get the number of CPU cores
+    num_cpus = multiprocessing.cpu_count()
+    
+    # You may adjust this formula based on your specific requirements
+    # For example, you could use num_cpus * 2 or any other heuristic
+    return num_cpus
+
+if __name__ == "__main__":
+    print(get_number_of_workers())
+
+
+"""
